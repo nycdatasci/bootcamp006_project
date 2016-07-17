4a12775128e6b2f7796a06392e49c69c191cd5c4
dkhurana1306 <deepak.khurana@nyu.edu>
2016-07-17 12:14:25 -0400

    yada yada yada

--- Project1-ExploreVis/Deepak_Khurana/dk_ExploreVis.R
+++ Project1-ExploreVis/Deepak_Khurana/dk_ExploreVis.R
@@ -1,12 +1,15 @@
 #Loading data from YELP Dataset Challenge (Round 7) 
 
-lbrary(jsonlite)                  # library to read data provided in .json file format
+library(jsonlite)                  # library to read data provided in .json file format
 
-checkin = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json"))   # read checkin data to checkin df 
-business = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json")) # read business data to business df
-tip = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_tip.json"))           # read tip data to tip df
-user = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json"))         # read user data to user df
-review = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json"))     # read review data to review df
+setwd('~/Downloads/yelp_dataset_challenge_academic_dataset/')
+
+checkin  = stream_in(file("yelp_academic_dataset_checkin.json"))   # read checkin data to checkin df 
+business = stream_in(file("yelp_academic_dataset_business.json"))  # read business data to business df
+tip      = stream_in(file("yelp_academic_dataset_tip.json"))       # read tip data to tip df
+user     = stream_in(file("yelp_academic_dataset_user.json"))      # read user data to user df
+review   = stream_in(file("yelp_academic_dataset_review.json"))    # read review data to review df
+
 
 
 
