e61ddcfa10dafd76e5ac0176746a06b10d0f5c5d
dkhurana1306 <deepak.khurana@nyu.edu>
2016-07-17 14:58:40 -0400

    yada yada yada

--- Project1-ExploreVis/Deepak_Khurana/dk_ExploreVis.R
+++ Project1-ExploreVis/Deepak_Khurana/dk_ExploreVis.R
@@ -4,12 +4,28 @@ library(jsonlite)                  # library to read data provided in .json file
 
 setwd('~/Downloads/yelp_dataset_challenge_academic_dataset/')
 
-checkin  = stream_in(file("yelp_academic_dataset_checkin.json"))   # read checkin data to checkin df 
-business = stream_in(file("yelp_academic_dataset_business.json"))  # read business data to business df
-tip      = stream_in(file("yelp_academic_dataset_tip.json"))       # read tip data to tip df
-user     = stream_in(file("yelp_academic_dataset_user.json"))      # read user data to user df
-review   = stream_in(file("yelp_academic_dataset_review.json"))    # read review data to review df
+#checkin  = stream_in(file("yelp_academic_dataset_checkin.json"))   # read checkin data to checkin df 
+business = stream_in(file("yelp_academic_dataset_business.json"))   # read business data to business df
+#tip      = stream_in(file("yelp_academic_dataset_tip.json"))       # read tip data to tip df
+#user     = stream_in(file("yelp_academic_dataset_user.json"))      # read user data to user df
+#review   = stream_in(file("yelp_academic_dataset_review.json"))    # read review data to review df
 
+
+
+#checkin_flat  = flatten(checkin)
+business_flat = flatten(business)
+#tip_flat      = flatten(tip)
+#user_flat     = flatten(user)
+#review_flat   = flatten(review)
+
+
+library(tibble)
+
+#checkin_tbl   = as_data_frame(checkin_flat)
+business_tbl  = as_data_frame(business_flat)
+#tip_tbl       = as_data_frame(tip_flat)
+#user_tbl      = as_data_frame(user_flat)
+#review_tbl    = as_data_frame(review_flat)
 
 
 
