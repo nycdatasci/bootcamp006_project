f928c882b8cce17d3adcaa8f6f6a51d2f418a0e4
dkhurana1306 <deepak.khurana@nyu.edu>
2016-07-17 11:02:19 -0400

    test test is anybody out there

---
+++ Project1-ExploreVis/Deepak_Khurana/dk_ExploreVis.R
@@ -0,0 +1,13 @@
+#Loading data from YELP Academic Challenge Dataset  
+
+lbrary(jsonlite)                  # library to read data provided in .json file format
+
+checkin = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json"))   # read checkin data to checkin df 
+business = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json")) # read business data to business df
+tip = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_tip.json"))           # read tip data to tip df
+user = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json"))         # read user data to user df
+review = stream_in(file("/Users/dk1306/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json"))     # read review data to review df
+
+
+
+
