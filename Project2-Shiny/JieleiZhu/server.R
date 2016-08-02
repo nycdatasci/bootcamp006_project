12784b045d49592baad9dcf4e47060c962c34caa
EmZhu <jielei.emma.zhu@gmail.com>
2016-08-02 10:23:11 -0400

    fixed crush

--- Project2-Shiny/JieleiZhu/server.R
+++ Project2-Shiny/JieleiZhu/server.R
@@ -1,38 +1,49 @@
 
-
 shinyServer(function(input, output){
   
-  # 
+  # show income data for never married, married(total) and divorced
+  output$marital_total_motion <-renderGvis({
+    gvisMotionChart(marital_total, 'Marital_Status', 'Year')
+  })
+  
+  # show income data for everything above plus spouse present and spouse absent
   output$marital_motion <- renderGvis({
     gvisMotionChart(marital_data, 'Marital_Status', 'Year')
   })
   
-  # 
+  # show income data for female and male
   output$gender_motion <- renderGvis({
     gvisMotionChart(gender_data, 'Gender', 'Year')
   })
   
-  # 
+  # show income data for white, black, asian, and hispanic
   output$race_motion <- renderGvis({
     gvisMotionChart(race_data, 'Race', 'Year')
   })
   
-  #
+  # show income data for white female, white male, black female, black male...
   output$race_gender_motion <- renderGvis({
     gvisMotionChart(race_gender_data, 'Race', 'Year')
   })
   
   # show histogram using googleVis
+  data <-reactive({
+    data = rename(melt(filter(hist_data,
+                  Marital_Status == input$marital_status,
+                  Year == input$year,
+                  Race == input$race,
+                  Gender == input$gender)[, !(colnames(hist_data) %in% optional_columns), drop=FALSE]
+  ))}) #, 'Population (in thousands)'=value)})
+  
   output$hist <- renderGvis({
-    gvisColumnChart(data = filter(hist_data,
-                                  Marital_Status == input$marital_status,
-                                  Year == input$year,
-                                  Race == input$race,
-                                  Gender == input$gender)[, !(colnames(hist_data) %in% optional_columns), drop=FALSE]
-    )
+    (a<- gvisColumnChart(data(), 
+                         options=list(fontSize = 16,
+                                      hAxis = "{title : 'Income categories'}",
+                                      vAxis = "{title : 'Population (in thousands)'}",
+                                      legend = "{position:'none'}",
+                                      width = 1200,
+                                      height = 700)))
   })
 })
-
-
 
 
--- Project2-Shiny/JieleiZhu/ui.R
+++ Project2-Shiny/JieleiZhu/ui.R
@@ -1,23 +1,12 @@
+library(shiny)
 
 shinyUI(
   navbarPage (
-    title = 'Relationship Between Income and Marital Status',
+    title = 'Explore your confidence using U.S. Income dataset',
     id = 'nav',
     theme = shinytheme('United'),
-    
-    tabPanel('Visualization',
-             fluidRow(
-               column(3,
-                      h4('')
-               ),
-               column(6,
-                      h3('Income vs. Marital status'),
-                      box(htmlOutput('marital_motion'), height = 500)
-               )
-             )
-    ),
-    
-    tabPanel('Raw',
+
+    tabPanel('Understand data',
              fluidRow(
                column(
                  width = 2,
@@ -26,74 +15,101 @@ shinyUI(
                  br(),
                  selectInput('marital_status',
                              'Select MARITAL STATUS',
-                             choices = list('NeverMarried' = 1,
-                                            'Married(Total)' = 2,
-                                            'SpousePresent' = 3,
-                                            'SpouseAway' = 4,
-                                            'Divorced' = 5),
-                             selected = 1),
+                             choices = marital_choice,
+                             selected = 'NeverMarried'),
                  br(),
                  selectInput('year',
                              'Select YEAR',
-                             choices = list('2005' = 1,
-                                            '2006' = 2,
-                                            '2007' = 3,
-                                            '2008' = 4,
-                                            '2009' = 5,
-                                            '2010' = 6,
-                                            '2011' = 7,
-                                            '2012' = 8,
-                                            '2013' = 9,
-                                            '2014' = 10),
-                             selected = 10),
+                             choices = year_choice,
+                             selected = '2014'),
                  br(),
                  selectInput('race',
                              'Select RACE',
-                             choices = list('White' = 1,
-                                            'Black' = 2,
-                                            'Asian' = 3,
-                                            'Hispanic' = 4),
-                             selected = 1),
+                             choices = race_choice,
+                             selected = 'White'),
                  br(),
                  selectInput('gender',
                              'Select GENDER',
-                             choices = list('Male' = 1,
-                                            'Female' = 2),
-                             selected = 1)
-                 
+                             choices = gender_choice,
+                             selected = 'Male')
                ),
                column(
                  width = 9,
-                 box(htmlOutput('hist'), height = 300)
+                 htmlOutput('hist')
                )
              )
     ),
-    tabPanel('More motion plots...',
-             fluidRow(
-               column(6, 
-                      h3('Income vs. Race'),
-                      box(htmlOutput('race_motion'), height = 500)
+    navbarMenu('Explore data',
+               tabPanel('Income vs. Marital Status',
+                        fluidRow(
+                          column(3,
+                                 h3(' ')
+                          ),
+                          column(6,
+                                 h3('Income vs. Marital Status'),
+                                 htmlOutput('marital_total_motion')
+                          )
+                        )
                ),
-               column(6, 
-                      h3('Income vs. Gender'),
-                      box(htmlOutput('gender_motion'), height = 500)
+               tabPanel('Income vs. Married(break down)',
+                        fluidRow(
+                          column(3,
+                                 h3(' ')
+                          ),
+                          column(6,
+                                 h3('Income vs. Marital Status'),
+                                 htmlOutput('marital_motion')
+                          )
+                        )
                ),
-               column(12,
-                      h3('Income vs Race+Gender'),
-                      box(htmlOutput('race_gender_motion'), height = 500)
+               tabPanel('Income vs. Gender',
+                        fluidRow(
+                          column(3,
+                                 h3(' ')
+                          ),
+                          column(6,
+                                 h3('Income vs. Gender'),
+                                 htmlOutput('gender_motion')
+                          )
+                        )
+               ),
+               tabPanel('Income vs. Race',
+                        fluidRow(
+                          column(3,
+                                 h3(' ')
+                          ),
+                          column(6,
+                                 h3('Income vs. Race'),
+                                 htmlOutput('race_motion')
+                          )
+                        )
+               ),
+               tabPanel('Income vs. Gender & Race',
+                        fluidRow(
+                          column(3,
+                                 h3(' ')
+                          ),
+                          column(6,
+                                 h3('Income vs. Gender & Race'),
+                                 htmlOutput('race_gender_motion')
+                          )
+                        )
                )
-             )
-    ),
-    tabPanel('Summary',
-             fluidPage(
-               column(3, img(src='wedding_money.jpg', height=400, width=300)),
-               column(6,
-                      h3('1. According to income data from 2005 to 2014, married couples reported the highest income on average, followed by people who are divorced, and people who stayed single.'),
-                      br(),
-                      br(),
-                      h3('2. When breaking down married people into "spouse present" and "spouse absent" subgroups, people who are married but does not live with their spouse reported lower income than people are divorced.')
-               )
-             )
     )
   )
 )
+
+# tabPanel('Summary',
+#          fluidPage(
+#            column(3, img(src='wedding_money.jpg', height=400, width=300)),
+#            column(6,
+#                   h3('1. According to income data from 2005 to 2014, married couples reported the highest income on average, followed by people who are divorced, and people who stayed single.'),
+#                   br(),
+#                   br(),
+#                   h3('2. When breaking down married people into "spouse present" and "spouse absent" subgroups, people who are married but does not live with their spouse reported lower income than people are divorced.')
+#            )
+#          )
+# )
+# )
+# )
+
