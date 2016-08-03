f3b997e720c0d73b161e2dce800fff154cf3b1cb
Shu Yan <yanshu.usc@gmail.com>
2016-08-03 14:31:49 -0400

    clean folders

--- Project2-Shiny/NandaRajarathinam/global.R
+++ Project2-Shiny/NandaRajarathinam/global.R
@@ -20,9 +20,9 @@ library(Hmisc)
 library(reshape2)
 
 #READ DATA
-# setwd("C:\\Users\\trichna\\Documents\\NYCDSA\\Shiny")
-#consumer_complaints = read.csv("C:\\Users\\trichna\\Documents\\NYCDSA\\Shiny\\Consumer_Complaints.csv",stringsAsFactors = FALSE)
-consumer_complaints = read.csv('.\\data\\Consumer_Complaints.csv', stringsAsFactors = F)
+# setwd("C:/Users/trichna/Documents/NYCDSA/Shiny")
+#consumer_complaints = read.csv("C:/Users/trichna/Documents/NYCDSA/Shiny/Consumer_Complaints.csv",stringsAsFactors = FALSE)
+consumer_complaints = read.csv('./data/Consumer_Complaints.csv', stringsAsFactors = F)
 #consumer_complaints   = consumer_complaints  %>% dplyr::rename( Timely.response = Timely.response., Consumer.disputed = Consumer.disputed. )   %>% select(Product, Sub.product, Issue, Company, State, ZIP.code, Date.sent.to.company, Company.response.to.consumer, Timely.response, Consumer.disputed, Complaint.ID)
 #consumer_complaints$Complaint_date = as.Date(consumer_complaints$ Date.sent.to.company,'%m/%d/%Y')
 #consumer_complaints$Year = year(consumer_complaints$Complaint_date)
@@ -32,16 +32,16 @@ consumer_complaints = read.csv('.\\data\\Consumer_Complaints.csv', stringsAsFact
 #consumer_compl_prod = consumer_compl_prod %>% filter(!State %in% c("PR", "AE", "MH", "AP", "FM", "VI", "GU", "MP", "AS", "PW", "AA",""))
 
 ########## Load data sets ##########
-# consumer_compl_by_prod_year <- readRDS(".\\data\\consumer_compl_by_prod_year.rds")
-consumer_compl_by_prod_year <- readRDS(".\\data\\consumer_compl_by_prod_year.rds")
-consumer_compl_by_prod_issue <- readRDS(".\\data\\consumer_compl_by_prod_issue.rds")
-top25_institution_data_merged_disp_desc <- readRDS(".\\data\\top25_institution_data_merged_disp_desc.rds")
-top25_institution_data_merged_sla_desc <- readRDS(".\\data\\top25_institution_data_merged_sla_desc.rds")
-consumer_disp_map_merged <- readRDS(".\\data\\consumer_disp_map_merged.rds")
-consumer_compl_map_merged_sla <- readRDS(".\\data\\consumer_compl_map_merged_sla.rds")
-reshaped_late_response <- readRDS(".\\data\\reshaped_late_response.rds")
-reshaped_disputed_response <- readRDS(".\\data\\reshaped_disputed_response.rds")
-reshaped_monetary_relief <- readRDS(".\\data\\reshaped_monetary_relief.rds")
+# consumer_compl_by_prod_year <- readRDS("./data/consumer_compl_by_prod_year.rds")
+consumer_compl_by_prod_year <- readRDS("./data/consumer_compl_by_prod_year.rds")
+consumer_compl_by_prod_issue <- readRDS("./data/consumer_compl_by_prod_issue.rds")
+top25_institution_data_merged_disp_desc <- readRDS("./data/top25_institution_data_merged_disp_desc.rds")
+top25_institution_data_merged_sla_desc <- readRDS("./data/top25_institution_data_merged_sla_desc.rds")
+consumer_disp_map_merged <- readRDS("./data/consumer_disp_map_merged.rds")
+consumer_compl_map_merged_sla <- readRDS("./data/consumer_compl_map_merged_sla.rds")
+reshaped_late_response <- readRDS("./data/reshaped_late_response.rds")
+reshaped_disputed_response <- readRDS("./data/reshaped_disputed_response.rds")
+reshaped_monetary_relief <- readRDS("./data/reshaped_monetary_relief.rds")
 
 
 
