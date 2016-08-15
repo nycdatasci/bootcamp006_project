42ccfe1f67a1d43d1b936226919f05b0941afdb8
David Richard Steinmetz <davidsteinmetz@gmail.com>
2016-08-14 23:17:31 -0400

    updated analysis

--- Project3-WebScraping/DavidSteinmetz/04_analysis.R
+++ Project3-WebScraping/DavidSteinmetz/04_analysis.R
@@ -161,6 +161,15 @@ g <- g + xlab('Age Group') + ylab('Time (s)')
 g <- g + scale_colour_discrete(name = "Age Category")
 g
 
+# --Age Group (Place)
+g <- ggplot(dt[!is.na(age.cat)], aes(as.factor(age.cat), place))
+g <- g + geom_boxplot(aes(colour=as.factor(age.cat)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Age Group') + ylab('Place')
+g <- g + scale_colour_discrete(name = "Age Category")
+g
+
 # --Age Group reordered according to median time
 g <- ggplot(dt[!is.na(age.cat)], aes(reorder(as.factor(age.cat), time, median), time))
 g <- g + geom_boxplot(aes(colour=as.factor(age.cat)))
@@ -233,6 +242,33 @@ var.test(dt[age.cat=='[17,35)'][['time']],
 # age groups is not the same and therefore the T-test is invalid.
 # 3. Obs. are randomly drawn and independent
 
+
+# Scatterplot: Age-Time
+g <- ggplot(dt, aes(age, time))
+g <- g + geom_point(aes(colour=as.factor(gender)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Age (years)') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Age")
+g
+
+# Scatterplot + Jitter: Age-Time
+g <- ggplot(dt, aes(age, time))
+g <- g + geom_jitter(aes(colour=as.factor(gender)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Age (years)') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Age")
+g
+
+# Scatterplot: Age-Place
+g <- ggplot(dt, aes(age, place))
+g <- g + geom_point(aes(colour=as.factor(gender)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Age (years)') + ylab('Place')
+g <- g + scale_colour_discrete(name = "Age")
+g
 
 
 # Time Group Analysis -----------------------------------------------------
@@ -308,11 +344,108 @@ g
 
 # Nationality Analysis ----------------------------------------------------
 
+nat.N <- head(dt[, .N, by = nat][order(-N)],10)  # Countries with the most participants
+nat <- subset(dt, nat %in% c('SUI', 'GER', 'NED', 'GBR', 'ITA'))
+
+# Barchart by nationality
+g <- ggplot(nat.N, aes(nat, N))
+g <- g + geom_bar(stat='identity')
+g <- g + theme_minimal()
+g <- g + ggtitle('Number of Participants by Nationality')
+g <- g + xlab('Country') + ylab('Number')
+g <- g + coord_flip()
+g
 
 
+# Boxplots
+# --Nationality
+g <- ggplot(nat, aes(as.factor(nat), time))
+g <- g + geom_boxplot(aes(colour=as.factor(nat)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Year') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Year")
+g
 
+# --Nationality reordered according to median time
+g <- ggplot(nat, aes(reorder(as.factor(nat), time, median), time))
+g <- g + geom_boxplot(aes(colour=as.factor(nat)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Nationality (ordered by median time)') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Nationality")
+g
 
+# --Nationality + Gender
+g <- ggplot(nat, aes(as.factor(nat), time))
+g <- g + geom_boxplot(aes(colour=gender))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Nationality') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Gender")
+g
 
+# --Gender + Nationality
+g <- ggplot(nat, aes(as.factor(gender), time))
+g <- g + geom_boxplot(aes(colour=reorder(nat, time)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Gender') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Nationality")
+g
+
+# --Nationality + Year
+g <- ggplot(nat, aes(as.factor(nat), time))
+g <- g + geom_boxplot(aes(colour=as.factor(year)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Nationality') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Year")
+g
+
+# --Nationality + Time Group
+g <- ggplot(nat, aes(as.factor(nat), time))
+g <- g + geom_boxplot(aes(colour=time.cat))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Nationality') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Time Group")
+g
+
+# --Nationality + Place Group
+g <- ggplot(nat, aes(as.factor(nat), time))
+g <- g + geom_boxplot(aes(colour=place.cat))
+g <- g + theme_minimal()
+g <- g + ggtitle('Distribution of Race Times')
+g <- g + xlab('Nationality') + ylab('Time (s)')
+g <- g + scale_colour_discrete(name = "Place Group")
+g
+
+# Density plots
+# --Nationality
+g <- ggplot(nat, aes(time))
+g <- g + geom_density(aes(colour = as.factor(nat)))
+g <- g + theme_minimal()
+g <- g + ggtitle('Density Distribution of Race Times')
+g <- g + xlab('Time (s)') + ylab('Density')
+g <- g + scale_colour_discrete(name = "Nationality")
+g
+
+# One-way ANOVA between Nationalities
+summary(aov(time ~ nat, data = dt))  # Sig. diff in mean time across ages
+# Check assumptions:
+# 1. Population is normally distributed
+# 2. SD of populations are equal
+nat[,.(SD_time = sd(time)), by = nat]
+t.test(dt[nat=='NED'][['time']], 
+       dt[nat=='GER'][['time']], 
+       alternative = 'two.sided')  # P-value 0.0007; means are different
+var.test(dt[nat=='NED'][['time']], 
+         dt[nat=='GER'][['time']], 
+         alternative = "two.sided") # P-value 0.092; T-test is VALID
+# Null hypothesis is kept: variance between
+# nationalities is the same and therefore the T-test is valid.
+# 3. Obs. are randomly drawn and independent
 
 
 
