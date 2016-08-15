b45bc799a99f6bbc3e83f228e65f3f5f01080829
EmZhu <jielei.emma.zhu@gmail.com>
2016-08-15 10:15:43 -0400

    commit again

---
+++ Project3-WebScraping/JieleiZhu/App/global.R
@@ -0,0 +1,47 @@
+if (!require(shinythemes)) install.packages('shinythemes')
+if (!require(shiny)) install.packages('shiny')
+if (!require(googleVis)) install.packages('googleVis')
+if (!require(dplyr)) install.packages('dplyr')
+if (!require(reshape2)) install.packages('reshape2')
+if (!require(shinydashboard)) install.packages("shinydashboard")
+if (!require(devtools)) install.packages('devtools')
+if (!require(shinysky)) devtools::install_github("AnalytixWare/ShinySky")
+library(shiny)
+library(shinythemes)
+library(shinysky)
+library(googleVis)
+library(reshape2)
+library(dplyr)
+
+
+#------------- load data
+my_read_csv = function (filename, 
+                         header = T, 
+                         colClasses = c('character', 
+                                        'character', 
+                                        'character', 
+                                        'Date', 
+                                        'integer', 
+                                        'integer', 
+                                        'character', 
+                                        'integer')) 
+  {
+  return(read.csv(filename, header = header, colClasses = colClasses))
+}
+
+without_duplicates = my_read_csv('Data/Without_duplicates.csv')
+with_duplicates = my_read_csv('Data/Deal_data.csv')
+
+popularity_overall = without_duplicates %>% group_by(Category, Store) %>% summarise(NumDeals = n(), Popularity = sum(Popularity))
+popularity_by_category = without_duplicates %>% group_by(Category) %>% summarise(Total = sum(Popularity))
+master_popularity_data = merge(popularity_overall, popularity_by_category, by = 'Category')
+master_popularity_data$PPD = master_popularity_data$Popularity / master_popularity_data$NumDeals
+
+most_store = master_popularity_data %>% group_by(Category) %>% select(Category, Store, NumberOfDeals = NumDeals) %>% top_n(10, NumberOfDeals)
+
+
+category_choices = unique(without_duplicates$Category)
+metric_choices = c('Both', 'Comments', 'Bookmarks')
+minimum_choices = c(1, 5, 10, 20, 50)
+store_names = unique(most_store$Store)
+
---
+++ Project3-WebScraping/JieleiZhu/App/server.R
@@ -0,0 +1,86 @@
+shinyServer(function(input, output){
+  output$hist <- renderGvis({
+    (a<- gvisColumnChart(data(), 
+                         options=list(fontSize = 16,
+                                      hAxis = "{title : 'Income categories'}",
+                                      vAxis = "{title : 'Population (in thousands)'}",
+                                      legend = "{position:'none'}",
+                                      width = 1200,
+                                      height = 700)))
+  })
+  
+  popular_data = reactive({
+    if (input$PopularMetric == 'Both') {
+      popular_data = without_duplicates %>% select(Category, Title, Description, Popularity = Popularity) %>% filter(Category == input$PopularCategory) 
+    }
+    else if(input$PopularMetric == 'Comments') {
+      popular_data = without_duplicates %>% select(Category, Title, Description, Popularity = NumberOfComments) %>% filter(Category == input$PopularCategory) 
+    }
+    else if(input$PopularMetric == 'Bookmarks') {
+      popular_data = without_duplicates %>% select(Category, Title, Description, Popularity = NumberOfBookmarks) %>% filter(Category == input$PopularCategory) 
+    }
+    
+    Popularity = popular_data$Popularity
+    return(popular_data[order(Popularity, decreasing = T), ])
+  })
+  
+  
+  
+  output$TopDeal = renderDataTable({
+    popular_data()
+  }, options = list(lengthMenu = c(5, 10, 15, 20), pageLength = 5))
+  
+  
+  output$PopularStore = renderDataTable({
+    temp = master_popularity_data %>% filter(Category == input$PopularStoreCategory) %>% filter(NumDeals >= input$MinimumDeals) %>% select(Category, Store, Popularity = PPD) 
+    PPD = temp$Popularity
+    return(temp[order(PPD, decreasing = T), ])
+    
+  }, options = list(lengthMenu = c(5, 10, 15, 20), pageLength = 5))
+  
+  
+  output$MostStore = renderDataTable({
+    NumDeals = most_store$NumberOfDeals
+    return(most_store[order(NumDeals, decreasing = T), ])
+  }, options = list(lengthMenu = c(5, 10, 15, 20), pageLength = 5))
+  
+  
+  output$timeline = renderGvis({
+    month_data = dplyr::summarise(group_by(filter(without_duplicates, Category == input$MostDealCategory), PostedTime, Category), Count = n())
+    
+    gvisCalendar(month_data, 
+                 datevar = "PostedTime", 
+                 numvar = "Count",
+                 options = list(
+                   title = "Monthly Frequency of Deals",
+                   height = 320,
+                   calendar = "{yearLabel: { fontName: 'Times-Roman',
+                          fontSize: 32, color: '#1A8763', bold: true},
+                          cellSize: 10,
+                          cellColor: { stroke: 'red', strokeOpacity: 0.2 },
+                          focusedCellColor: {stroke:'red'}}")
+    )
+  })
+  
+  output$annotation = renderGvis({
+    # temp = without_duplicates %>% dplyr::filter(Category == input$SingleMostCategory, Store == input$DingleMostStore) %>% dplyr::group_by(PostedTime, Category) %>% dplyr::summarise(Count = n())
+
+
+    gvisAnnotationChart(temp,
+                        datevar="PostedTime",
+                        numvar="Count",
+                        # idvar="Device",
+                        # titlevar="Title",
+                        # annotationvar="Annotation",
+                        options=list(
+                          width=600, height=350,
+                          fill=10, displayExactValues=TRUE,
+                          colors="['#0000ff']")
+    )
+  })
+
+  
+})
+
+
+
