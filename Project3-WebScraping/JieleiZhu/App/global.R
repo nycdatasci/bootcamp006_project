134704973e14b70ecfe6eb4f43d431490bd319af
EmZhu <jielei.emma.zhu@gmail.com>
2016-08-15 14:48:59 -0400

    update

--- Project3-WebScraping/JieleiZhu/App/global.R
+++ Project3-WebScraping/JieleiZhu/App/global.R
@@ -27,12 +27,14 @@ if (!require(reshape2)) install.packages('reshape2')
 if (!require(shinydashboard)) install.packages("shinydashboard")
 if (!require(devtools)) install.packages('devtools')
 if (!require(shinysky)) devtools::install_github("AnalytixWare/ShinySky")
+if (!require(lubridate)) install.packages('lubridate')
 library(shiny)
 library(shinythemes)
 library(shinysky)
 library(googleVis)
 library(reshape2)
 library(dplyr)
+require(lubridate)
 
 
 #------------- load data
@@ -63,17 +65,22 @@ with_duplicates = my_read_csv('Data/Deal_data2.csv')
 without_duplicates = my_read_csv('Data/Without_duplicates.csv')
 with_duplicates = my_read_csv('Data/Deal_data.csv')
 
-popularity_overall = without_duplicates %>% group_by(Category, Store) %>% summarise(NumDeals = n(), Popularity = sum(Popularity))
-popularity_by_category = without_duplicates %>% group_by(Category) %>% summarise(Total = sum(Popularity))
+popularity_overall = without_duplicates %>% group_by(Category, Store) %>% dplyr::summarise(NumDeals = n(), Popularity = sum(Popularity))
+popularity_by_category = without_duplicates %>% group_by(Category) %>% dplyr::summarise(Total = sum(Popularity))
 master_popularity_data = merge(popularity_overall, popularity_by_category, by = 'Category')
 master_popularity_data$PPD = master_popularity_data$Popularity / master_popularity_data$NumDeals
 
-most_store = master_popularity_data %>% group_by(Category) %>% select(Category, Store, NumberOfDeals = NumDeals) %>% top_n(10, NumberOfDeals)
+most_store = master_popularity_data %>% select(Category, Store, NumberOfDeals = NumDeals)
 
+most_store_temp = most_store %>% group_by(Category) %>% top_n(10, NumberOfDeals)
 
 category_choices = unique(without_duplicates$Category)
-metric_choices = c('Both', 'Comments', 'Bookmarks')
+metric_choices = c('Both', 'By Comments', 'By Bookmarks')
 minimum_choices = c(1, 5, 10, 20, 50)
+<<<<<<< HEAD
+store_names = unique(most_store_temp$Store)
+=======
 store_names = unique(most_store$Store)
 >>>>>>> d3eae80b1667a0750991df3a7dcd9151a194eac1
+>>>>>>> 54fdc9012e13a46dc1f29c146d3564545c7b6ec1
 
--- Project3-WebScraping/JieleiZhu/App/ui.R
+++ Project3-WebScraping/JieleiZhu/App/ui.R
@@ -1,208 +1,165 @@
 library(shiny)
 
-shinyUI(
-<<<<<<< HEAD
-  navbarPage (
-    title = 'Finding deals (Created by Emma(Jielei) Zhu)',
-    id = 'nav',
-    theme = shinytheme('Readable'),
-    
-    tabPanel('About',
-             fluidRow(
-               column(width = 2
-                      
-               )
-             )
-    ),
-    navbarMenu('Past Data',
-               tabPanel('Clothing, Jewelry & Bags',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Beauty',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Nutrition',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Baby',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('At Home',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Electronics',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Travel',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               ),
-               tabPanel('Finance',
-                        fluidRow(
-                          column(width = 6
-                          )
-                        )
-               )
-    ),
-    tabPanel('Prediction',
-             fluidRow(
-               column(width = 2
-                      
-               )
-             )
-=======
-  dashboardHeader(title = 'Better understand deals (Created by Emma(Jielei) Zhu)'),
-  sidebarMenu(
-    menuItem('About', tabName = "about", icon = icon("dashboard")),
-    menuItem('What deals are popular?', tabName = "popular deals", icon = icon("dashboard")),
-    menuItem('Which stores have popular deals?', tabName = "popular stores",icon = icon("dashboard")),
-    menuItem('Which stores have the most deals?', tabName = "store most deals", icon = icon("th")),
-    menuItem('When are there most deals?', tabName = "time most deals", icon = icon("th")),
-    menuItem('Prediction', tabName = "prediction", icon = icon("th"))
+dashboardPage(
+  skin = "green",
+  dashboardHeader(
+    title = "Where are the good deals? --Created by Emma(Jielei) Zhu",
+    titleWidth = 600
   ),
-  dashboardPage(skin = "black"),
+  dashboardSidebar(
+    width = 320,
+    sidebarMenu(
+      menuItem(h4('About the data'), tabName = "about", icon = icon("dashboard")),
+      menuItem(h4('WHAT DEALS are popular?'), tabName = "popular_deals", icon = icon("money")),
+      menuItem(h4('Which stores have POPULAR deals?'), tabName = "popular_stores",icon = icon("shopping-bag")),
+      menuItem(h4('Which stores have the MOST deals?'), tabName = "store_most_deals", icon = icon("shopping-basket")),
+      menuItem(h4('WHEN are there most deals?'), tabName = "time_most_deals", icon = icon("shopping-cart")),
+      menuItem(h4('Prediction'), tabName = "prediction", icon = icon("flask"))
+    )),
+  # dashboardPage(),
   dashboardBody(
     tabItems(
-      tabItem(
-              tabItem(tabName = 'about',
-                      fluidRow(
-                        column(width = 2
-                               
-                        )
-                      )
-              ),
-              tabItem(tabName = 'popular deals',
-                      fluidRow(
-                        style = "background-color:#F8F8F8",
-                        column(width = 2,
-                               selectInput('PopularCategory',
-                                           'Select Category',
-                                           choices = category_choices,
-                                           selected = 'Baby'),
-                               selectInput('PopularMetric',
-                                           'Select Popularity Metric',
-                                           choices = metric_choices,
-                                           selected = 'Both')
-                        ),
-                        column(width = 8,
-                               offset = 3,
-                               h2('Most Popular Deals'),
-                               dataTableOutput('TopDeal')
-                               
-                        )
-                      )   
-                      
-              ),
-              tabItem(tabName = 'popular stores',
-                       fluidRow(
-                         style = "background-color:#F8F8F8",
-                         column(width = 2,
-                                selectInput('PopularStoreCategory',
-                                            'Select Category',
-                                            choices = category_choices,
-                                            selected = 'Baby'),
-                                selectInput('MinimumDeals',
-                                            'Select Minimum Number of Deals',
-                                            choices = minimum_choices,
-                                            selected = 5)
-                         ),
-                         column(width = 8,
-                                offset = 3,
-                                h2('Popular Stores'),
-                                dataTableOutput('PopularStore')
-                                
-                         )
-                       )   
-                       
-              ),
-              tabItem(tabName = 'store most deals',
-                       fluidRow(
-                         style = "background-color:#F8F8F8",
-                         column(width = 2,
-                                selectInput('MostStoreCategory',
-                                            'Select Category',
-                                            choices = category_choices,
-                                            selected = 'Baby')
-                         ),
-                         column(width = 8,
-                                offset = 3,
-                                h2('Stores with Most Deals'),
-                                dataTableOutput('MostStore')
-                                
-                         )
-                       )   
-              ),
-              tabItem(tabName = 'time most deals',
-                       fluidRow(
-                         # style = "background-color:#F8F8F8",
-                         column(width = 2,
-                                selectInput('MostDealCategory',
-                                            'Select Category',
-                                            choices = category_choices,
-                                            selected = 'Baby')
-                         )
-                       ),
-                       fluidRow(
-                         column(width = 8,
-                                offset = 3,
-                                h2('Timeline'),
-                                htmlOutput('timeline')
-                         )
-                       ),
-                       fluidRow(
-                         # style = "background-color:#F8F8F8",
-                         column(width = 2,
-                                selectInput('SingleMostCategory',
-                                            'Select Category',
-                                            choices = category_choices,
-                                            selected = 'Baby'),
-                                textInput.typeahead('SingleMostStore',
-                                                    placeholder="Type store name here",
-                                                    local=data.frame(name = store_names),
-                                                    valueKey = "name",
-                                                    tokens=c(1:length(store_names)),
-                                                    template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>")
-                                )
-                         ),
-                         column(width = 8,
-                                offset = 3,
-                                h2('Timeline'),
-                                htmlOutput('annotation')
-                                
-                         )
-                       ) 
-              ),
-              tabItem(tabName = 'prediction',
-                       fluidRow(
-                         column(width = 8,
-                                offset = 2,
-                                h2('How to infer which deals are good deals?')
-                                
+      tabItem(tabName = 'about',
+              fluidRow(
+                column(width = 11,
+                       offset = 1,
+                       h1(
+                         tags$ul(
+                           tags$li('All data are crawled from ', 
+                                   a(href="https://dealmoon.com", ' dealmoon.com')), 
+                           br(),
+                           tags$li("Total of ", span("~45,000", style = 'color:red'), 
+                                   " deals from ", 
+                                   span("8", style = 'color:blue'), " categories (i.e. ", 
+                                   span("Clothing, Beauty, Nutrition, Baby, Home, Electronics, Travel, Finance", style='color:blue'), " )"), 
+                           br(),
+                           tags$li("Total of ", span("6", style = 'color:green'), " attributes (i.e.", span("category of deal, deal title, deal description, posted time, number of comments, number of bookmarks", style = 'color:green')),
+                           br(),
+                           tags$li("The entire crawling process took ~6hrs."),
+                           br(),
+                           img(src='dealmoon.png', height=330, width=800)
                          )
                        )
+                )
+              )
+      ),
+      tabItem(tabName = 'popular_deals',
+              fluidRow(
+                br(),br(),br(),
+                column(width = 3,
+                       selectInput('PopularCategory',
+                                   h3('Select Category'),
+                                   choices = category_choices,
+                                   selected = 'Baby')
+                ),
+                column(width = 3,
+                       selectInput('PopularMetric',
+                                   h3('Select Popularity Metric'),
+                                   choices = metric_choices,
+                                   selected = 'By Bookmarks')
+                ),
+                box(
+                  width = 12,
+                  h2('Most Popular Deals'),
+                  dataTableOutput('TopDeal')
+                  
+                )
+              )
+              
+      ),
+      tabItem(tabName = 'popular_stores',
+              fluidRow(
+                br(),br(),
+                column(width = 3,
+                       selectInput('PopularStoreCategory',
+                                   h3('Select Category'),
+                                   choices = category_choices,
+                                   selected = 'Baby')
+                ),
+                column(width = 9,
+                       selectInput('MinimumDeals',
+                                   h3('Select Minimum Number of Deals a Store Must Have'),
+                                   choices = minimum_choices,
+                                   selected = 5)
+                ),
+                box(
+                  width = 12,
+                  h2('Popular Stores'),
+                  dataTableOutput('PopularStore')
+                  
+                )
+              )   
+              
+      ),
+      tabItem(tabName = 'store_most_deals',
+              fluidRow(
+                column(width = 3,
+                       br(),br(),
+                       selectInput('MostStoreCategory',
+                                   h3('Select Category'),
+                                   choices = category_choices,
+                                   selected = 'Baby')
+                )
+              ),
+              fluidRow(
+                box(width = 12,
+                       h2('Stores with Most Deals'),
+                       dataTableOutput('MostStore')
+                )
+              )
+      ),
+      tabItem(tabName = 'time_most_deals',
+              fluidRow(
+                column(width = 3,
+                       selectInput('MostDealCategory',
+                                   h3('Select Category'),
+                                   choices = category_choices,
+                                   selected = 'Baby')
+                ),
+                column(width = 6,
+                       offset = 1,
+                       h1('Deal Frequency By year', align = 'center')
+                       )
+              ),
+              fluidRow(
+                column(width = 9,
+                       offset = 3,
+                       htmlOutput('calendar')
+                )
+              ),
+              br(),br(),br(),br(),br(),br(),
+              fluidRow(
+                box(width = 4,
+                    selectInput('SingleMostCategory',
+                                h3('Select Category'),
+                                choices = category_choices,
+                                selected = 'Baby',
+                                width = '300px'),
+                    textInput.typeahead('SingleMostStore',
+                                        placeholder="Type store name here",
+                                        local=data.frame(name = store_names),
+                                        valueKey = "name",
+                                        tokens=c(1:length(store_names)),
+                                        template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>")
+                    )
+                ),
+                box(width = 8,
+                    h2('Timeline'),
+                    htmlOutput('annotation')
+                )
+              ),
+              br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br()
+      ),
+      tabItem(tabName = 'prediction',
+              fluidRow(
+                column(width = 8,
+                       offset = 2,
+                       br(),br(),br(),br(),
+                       h2("Can't dothe answer is not in this dataset.", align = 'center')
+                       
+                )
               )
       )
->>>>>>> d3eae80b1667a0750991df3a7dcd9151a194eac1
     )
   )
 )
