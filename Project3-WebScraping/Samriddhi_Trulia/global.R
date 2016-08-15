f5d3659e7b906e88d3f3801c8feeec382c447541
sam648 <sam648shakya@gmail.com>
2016-08-14 22:57:19 -0400

    scrappy project Trulia Samriddhi

---
+++ Project3-WebScraping/Samriddhi_Trulia/global.R
@@ -0,0 +1,167 @@
+
+
+library(stringr)
+library(leaflet)
+library(dplyr)
+library(htmltools)
+require(reshape2)
+require(googleVis)
+
+setwd("C:/Users/Samriddhi/Desktop/NYCDataScience/Python/Trulia")
+
+ApartmentComplex = read.csv("ApartmentsComplexes.csv")
+IndividualApartment = read.csv("Apartments.csv")
+
+Central_Harlem = c(	10026, 10027, 10030, 10037, 10039)
+Chelsea_Clinton	 = c(10001, 10011, 10018, 10019, 10020, 10036)
+East_Harlem = c	(10029, 10035)
+GramercyPark_MurrayHill = c(	10010, 10016, 10017, 10022)
+GreenwichVillage_Soho =c (	10012, 10013, 10014)
+Lower_Manhattan = c(	10004, 10005, 10006, 10007, 10038, 10280)
+Lower_EastSide =c(	10002, 10003, 10009 )
+Upper_EastSide =c (	10021, 10028, 10044, 10065, 10075, 10128)
+Upper_WestSide	= c(10023, 10024, 10025)
+Inwood_WashingtonHeights	= c(10031, 10032, 10033, 10034, 10040)
+
+
+ApartmentComplex2 = ApartmentComplex %>%
+  mutate(Neighborhood = ifelse(Postal %in%  Central_Harlem, "Central Harlem",
+  ifelse(Postal %in%  Chelsea_Clinton, "Chelsea Clinton",
+  ifelse(Postal %in%  East_Harlem, "East Harlem",
+  ifelse(Postal %in%  GramercyPark_MurrayHill, "GramercyPark MurrayHill",
+  ifelse(Postal %in%  GreenwichVillage_Soho, "GreenwichVillage Soho",
+  ifelse(Postal %in%  Lower_Manhattan, "Lower Manhattan",
+  ifelse(Postal %in%  Lower_EastSide, "Lower EastSide",
+  ifelse(Postal %in%  Upper_EastSide, "Upper EastSide",
+  ifelse(Postal %in%  Upper_WestSide, "Upper_WestSide",
+  ifelse(Postal %in%  Inwood_WashingtonHeights, "Inwood WashingtonHeights",
+  "Unknown" )))))))))))
+
+ApartmentComplex2 = ApartmentComplex2[ApartmentComplex2$Neighborhood !="Unknown", ]
+
+IndividualApartment2 = IndividualApartment %>%
+  mutate(Neighborhood = ifelse(Postal %in%  Central_Harlem, "Central Harlem",
+  ifelse(Postal %in%  Chelsea_Clinton, "Chelsea Clinton",
+  ifelse(Postal %in%  East_Harlem, "East Harlem",
+  ifelse(Postal %in%  GramercyPark_MurrayHill, "GramercyPark MurrayHill",
+  ifelse(Postal %in%  GreenwichVillage_Soho, "GreenwichVillage Soho",
+  ifelse(Postal %in%  Lower_Manhattan, "Lower Manhattan",
+  ifelse(Postal %in%  Lower_EastSide, "Lower EastSide",
+  ifelse(Postal %in%  Upper_EastSide, "Upper EastSide",
+  ifelse(Postal %in%  Upper_WestSide, "Upper_WestSide",
+  ifelse(Postal %in%  Inwood_WashingtonHeights, "Inwood WashingtonHeights",
+  "Unknown" )))))))))))
+IndividualApartment2 = IndividualApartment2[IndividualApartment2$Neighborhood !="Unknown", ]
+
+IndividualApartment2$NoOfBedroom = gsub("bd","",IndividualApartment2$NoOfBedroom)
+IndividualApartment2$NoOfBathRoom = gsub("ba","",IndividualApartment2$NoOfBathRoom)
+IndividualApartment2$Area = gsub("sqft","",IndividualApartment2$Area)
+
+
+IndividualApartment2$NoOfBedroom  = as.numeric(IndividualApartment2$NoOfBedroom)
+IndividualApartment2$NoOfBathRoom  = as.numeric(IndividualApartment2$NoOfBathRoom)
+IndividualApartment2$Area  = as.numeric(IndividualApartment2$Area)
+
+IndividualApartment2$Neighborhood = as.factor(IndividualApartment2$Neighborhood)
+
+
+
+IndividualApartment2 = IndividualApartment2 %>%
+  mutate(Bedroom_number = 
+                      ifelse(IndividualApartment2$NoOfBedroom == 1, "1",
+                      ifelse(IndividualApartment2$NoOfBedroom == 2, "2",
+                      ifelse(IndividualApartment2$NoOfBedroom == 3, "3",
+                      "3+" ))))
+
+IndividualApartment2 = IndividualApartment2 %>%
+  mutate(Bedroom_number = ifelse(is.na(Bedroom_number),"Null",Bedroom_number))
+
+
+
+
+#-----------------------------------------------------------------------------------------------------
+
+
+Apt_Bedrooms_rent =IndividualApartment2 %>% 
+  group_by(Bedroom_number) %>%
+  summarise( TotalApartments = n(), AvgRent = mean(RentRate) )
+
+
+
+#-----------------------------------------------------------------------------------------------------
+
+# Apartment No of Bedrrom, Rent price, Neighboorhood
+Apt_Bedrooms_rent_Neighborhood =IndividualApartment2 %>% 
+  group_by(Neighborhood, Bedroom_number) %>%
+  summarise( TotalApartments = n(), AvgRent = mean(RentRate) )
+
+
+Apt_Bedrooms_rent_Neighborhood_2 = dcast(Apt_Bedrooms_rent_Neighborhood,
+                                         Neighborhood ~ Bedroom_number, value.var= "TotalApartments")
+
+Apt_Bedrooms_rent_Neighborhood_3 = dcast(Apt_Bedrooms_rent_Neighborhood,
+                                         Neighborhood ~ Bedroom_number, value.var= "AvgRent")
+
+
+#-----------------------------------------------------------------------------------------------------
+
+
+Apt_Crime_Neighborhood =   IndividualApartment2 %>% 
+  group_by(Neighborhood, CrimeLevel) %>%
+  summarise( frequency = n() )
+
+
+#dcast
+Apt_Crime_Neighborhood_2 = dcast(Apt_Crime_Neighborhood,
+                                 Neighborhood ~ CrimeLevel, value.var= "frequency")
+
+
+
+#-----------------------------------------------------------------------------------------------------
+
+
+
+Apt_ElementarySchool_Neighborhood =   IndividualApartment2 %>% 
+  group_by(Neighborhood, ElementarySchool_rating) %>%
+  summarise( frequency = n() )
+
+
+#dcast
+Apt_ElementarySchool_Neighborhood_2 = dcast(Apt_ElementarySchool_Neighborhood,
+                                            Neighborhood ~ ElementarySchool_rating, value.var= "frequency")
+
+names(Apt_ElementarySchool_Neighborhood_2)[5] = "Not Mentioned"
+names(Apt_ElementarySchool_Neighborhood_2)
+
+
+#------------------------------------------------------------------------------------------------------
+
+Apt_MiddleSchool_Neighborhood =   IndividualApartment2 %>% 
+  group_by(Neighborhood, MiddleSchool_rating) %>%
+  summarise( frequency = n() )
+
+
+#dcast
+Apt_MiddleSchool_Neighborhood_2 = dcast(Apt_MiddleSchool_Neighborhood,
+                                        Neighborhood ~ MiddleSchool_rating, value.var= "frequency")
+
+names(Apt_MiddleSchool_Neighborhood_2)[5] = "Not Mentioned"
+names(Apt_MiddleSchool_Neighborhood_2)
+
+
+#--------------------------------------------------------------------------------------------------------
+
+
+Apt_HighSchool_Neighborhood =   IndividualApartment2 %>% 
+  group_by(Neighborhood, HighSchool_rating) %>%
+  summarise( frequency = n() )
+
+
+#dcast
+Apt_HighSchool_Neighborhood_2 = dcast(Apt_HighSchool_Neighborhood,
+                                      Neighborhood ~ HighSchool_rating, value.var= "frequency")
+
+names(Apt_HighSchool_Neighborhood_2)[5] = "Not Mentioned"
+names(Apt_HighSchool_Neighborhood_2)
+
+#-----------------------------------------------------------------------------------------------------
